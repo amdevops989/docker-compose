358384fe74ffe54ee63f881b96a6df4ea333bd6c
amdevops989 <am.devops989@gmail.com>
2026-06-07 15:56:21 -0400

    PodMonitor

--- Infra_AWS/modules/6-prometheus/terraform.tfvars
+++
@@ -1,1 +0,0 @@
-# slack_webhook_url = "https://hooks.slack.com/services/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"
--- Infra_AWS/modules/data-infra/debezium/connector/cmd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Injecting config"
-curl -X POST http://localhost:8083/connectors \
-     -H "Content-Type: application/json" \
-     -d @pg-connector.json
-
-echo "Checking status"
-curl http://localhost:8083/connectors/pg-auth-catalog-orders/status
